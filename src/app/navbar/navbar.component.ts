e1e23d98948292b65d19ce6d5cc2c4a37af70d03
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-03-30 13:54:08 -0300

    refactor: dynamically populated links

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+type LinkObject = {
+  content: string
+  route: string
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  links: LinkObject[] = [
+    {content: 'Home', route: 'courses'},
+    {content: 'Courses', route: 'courses'},
+  ]
 
   constructor() { }
 
