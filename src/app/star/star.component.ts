f5042c21538d9cb3382c14cd8dfe84e6bc6a30f1
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-03-30 09:54:43 -0300

    refactor: star component change according rating

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
-  template: `
-    <p>
-      star works!
-    </p>
-  `,
+  templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnChanges {
+
+  @Input()
+  rating: number = 0;
+
+  starWidth: number = 0 ;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 74 / 5;
   }
+
+
 
 }
