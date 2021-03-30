1004a26eafb7b862e3ace2d7a84b50265c4d921a
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-03-30 09:23:10 -0300

    feat: create star component

---
+++ src/app/star/star.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  template: `
+    <p>
+      star works!
+    </p>
+  `,
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
